e6c1a4b32594b2476ab19f3d64bafce8297cb9ba
mnsmns48 <115641157+mnsmns48@users.noreply.github.com>
2023-01-19 17:19:04 +0300

    Delete OLD FILES directory

--- OLD FILES/add_in_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('ktk.json') as f:
-    templates = json.load(f)
-
-print(len(templates[0]))
--- OLD FILES/main.py
+++
@@ -1,4 +0,0 @@
-# from window import Window
-#
-# window = Window(300, 500)
-# window.run()
